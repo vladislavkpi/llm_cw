0feab1519d8aa7ab4333e136e5cb801ff43c62c5
vladislavkpi <vladislav.kpi@gmail.com>
2025-02-11 23:12:35 +0200

    Update forecast.py

--- app/forecast.py
+++ app/forecast.py
@@ -1,1 +1,31 @@
+import os
+import yfinance as yf
+import pandas as pd
+import requests
 
+# Функція для отримання історичних даних та підготовки часових рядів
+
+def get_time_series_data(ticker):
+    stock = yf.Ticker(ticker)
+    price_data = stock.history(period="2y", interval="1mo")[['Close']]
+    price_data.reset_index(inplace=True)
+    price_data.rename(columns={"Date": "date", "Close": "stock_price"}, inplace=True)
+    # Перетворення стовпця дати у формат datetime
+    price_data["date"] = pd.to_datetime(price_data["date"])
+    return price_data
+
+# Функція для прогнозування ціни акцій
+
+def predict_stock_price(df):
+    if df is None or df.empty:
+        return {"error": "Not enough data for forecasting"}
+    
+    forecast = nixtla_client.forecast(
+        df=df,
+        freq='MS',
+        time_col='date',
+        target_col='stock_price',
+        h=3,
+        #level=[80, 90]
+    )
+    return float(forecast[2:3]['TimeGPT'])
