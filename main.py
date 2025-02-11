ae54948d56dea3f5b086c061c5db68658fa9692d
vladislavkpi <vladislav.kpi@gmail.com>
2025-02-11 22:58:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# імпортуємо бібліотеки
 from openai import OpenAI
 import os
 from fastapi import FastAPI
@@ -9,4 +10,31 @@ import nest_asyncio
 import uvicorn
 import requests
 import json
+#from google.colab import userdata #треба у випадку роботи з колабом
+
+# ініціюємо АПІ
+app = FastAPI()
+
+class StockRequest(BaseModel):
+    ticker: str
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Financial Analysis API"}
+
+# пропишемо ключі, якщо працюємо через Колаб
+client = OpenAI(
+  api_key=userdata.get('api_key')
+)
+nixtla_client = NixtlaClient(
+    api_key = userdata.get('TIMEGPT_API_KEY')
+)
+
+# пропишемо ключі, якщо працюємо через Python
+client = OpenAI(
+  api_key=OPENAI_API_KEY
+)
+nixtla_client = NixtlaClient(
+    api_key = TIMEGPT_API_KEY
+)
 
